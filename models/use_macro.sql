6310b50e60925254aa16a6e09cbf90cccb5fc45a
Aura Frizzati <auraf85@yahoo.it>
2025-02-23 18:21:20 +0000

    created and used macro

---
+++ models/use_macro.sql
@@ -0,0 +1,15 @@
+
+SELECT
+    o.Order_ID,
+    c.Customer_ID,
+    c.Customer_Name,
+    p.Product_ID,
+    p.Product_Name,
+    o.Quantity,
+    {{get_date_parts('Order_Date')}} as date_extract
+FROM
+    data-eng-week3-af.dbt_feb_AF.Orders o
+JOIN
+    {{ ref("stg_customers") }} c ON o.Customer_ID = c.Customer_ID
+JOIN
+    data-eng-week3-af.dbt_feb_AF.Products p ON o.Product_ID = p.Product_ID
